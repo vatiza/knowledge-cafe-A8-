8570720b6f9422dc04d1f20ec94cd1644516a88a
vatiza <rakibur.mac@gmail.com>
2024-02-20 22:09:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Nav from './Components/Nav/Nav'
+
+function App() {
+
+
+  return (
+    <>
+     <Nav></Nav>
+     <div>
+      
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Nav/Nav.jsx
@@ -0,0 +1,19 @@
+import profile from '../../assets/images/profile.png';
+import './Nav.css'
+
+const Nav = () => {
+    return (
+        <div className='nav'>
+            <h1>Knowledge Cafe</h1>
+            <ul>
+                <a href="">Home</a>
+                <a href="">Notification</a>
+                <a href="">Message</a>
+                <a href="">About</a>
+                <img src={profile} alt="" />
+            </ul>
+        </div>
+    );
+};
+
+export default Nav;
